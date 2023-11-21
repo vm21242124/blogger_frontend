1d6b497647aa227be09b6b02dd1acdadd3500d0e
vm21242124 <vm21242124@gmail.com>
2023-11-21 12:48:14 +0530

    routing in profilepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import UserRegister from "./pages/UserRegister"
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom"
 import HomePage from "./pages/HomePage/HomePage"
+import { useSelector } from "react-redux"
+import { useEffect } from "react"
 
 function App() {
+  const user =useSelector(state=>state.user)
+  const nav=useNavigate()
+  useEffect(()=>{
+    if(user===null){
+      nav("/auth")
+    }
+  },[user])
   return(
   <>
   <HomePage/>
--- src/Componenets/RightNav.jsx
+++ src/Componenets/RightNav.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { tempost, userdata } from '../Tempdata'
+import { useDispatch, useSelector } from 'react-redux'
+import { setActiveComp } from '../ReduxSetup/ActiveComponentSlice'
 
-const RightNav = () => {
+const RightNav = ({setDisplay}) => {
+  const user=useSelector(state=>state.user)
+  const dispatch =useDispatch()
   return (
     <div className="right">
         <div className="userprofile">
-            <img className="profilepic" src={userdata.profilepic}/>
-            <span>{userdata.name}</span>
+            <img className="profilepic" src={`http://localhost:8080/img/${user?.profilepic}`}/>
+            <span onClick={()=>{dispatch(setActiveComp("profile"));setDisplay("profile")}}>{user?.name}</span>
             <span className='followbtn'>switch</span>
         </div>
         <span style={{marginTop:"10px",marginLeft:"5px",color:"rgb(225,225,225"}}>Suggested For You</span>
